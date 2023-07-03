939bfba1f9e873377223faea9e598a07ef885076
tsc <tscasady@gmail.com>
2023-07-03 17:56:37 -0500

    fix binary search

--- neetcode/src/binary_search.rs
+++ neetcode/src/binary_search.rs
@@ -1,16 +1,35 @@
 pub fn binary_search(nums: Vec<i32>, target: i32) -> i32 {
     let mut low = 0;  
-    let mut high = nums.len() - 1;
+    let mut high = nums.len() as i32 - 1;
 
-    loop {
+    while low <= high {
         let mid = (high - low) / 2 + low;
 
-        if target == nums[mid] {
-            return mid as i32
-        } else if target < nums[mid] {
+        if target == nums[mid as usize] {
+            return mid
+        } else if target < nums[mid as usize] {
             high = mid - 1
         } else {
-            low = mid
+            low = mid + 1
         }
     }    
+    -1
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn binary_search_test() {
+        let nums = vec![-1,0,3,5,9,12];
+        assert_eq!(binary_search(nums, 2), -1);
+    }
+
+    #[test]
+    fn binary_search_one_element() {
+        let nums = vec![5];
+        assert_eq!(binary_search(nums, -5), -1);
+    }
 }
